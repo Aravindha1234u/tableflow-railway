c58955adb5d297c1ab14d3dfd45e3e7a8dd6ddc5
Aravindha1234u <aravindha1234u@gmail.com>
2023-09-01 22:21:47 -0400

    Fixed nginx configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start the backend service
-/backend/build &
-
 # Start the nginx process
-nginx -g "daemon off;"
+nginx -g "daemon on;"
+
+# Start the backend service
+/backend/build
